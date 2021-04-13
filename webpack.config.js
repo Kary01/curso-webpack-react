d015bf467149fde4ad8b5bac21d97b18f17a05b4
Karen Ruiz <anakaren.ruizb@gmail.com>
2021-04-12 20:30:06 -0500

    add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -25,13 +26,24 @@ module.exports = {
                     loader: 'html-loader'
                 }
             },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: './index.html',
-        }) 
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
     ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
