a74eb56c6f44a45565785d48e5d8e1a8765677c4
Karen Ruiz <anakaren.ruizb@gmail.com>
2021-04-12 21:28:52 -0500

    add optimization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/global.scss';
+import 'styles/global.scss';
 
-import App from './components/App';
+import App from 'components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
